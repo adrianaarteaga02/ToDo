1eec6d8d94be03847c33c01051b179f7ec4e3696
Adriana Arteaga <spamadri007@gmail.com>
2019-09-30 17:50:11 +0200

    Security done

--- src/main/java/ch/zli/m223/todo/ToDoApplication.java
+++ src/main/java/ch/zli/m223/todo/ToDoApplication.java
@@ -1,4 +1,14 @@
 package ch.zli.m223.todo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ToDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoApplication.class, args);
+    }
+
+
 }
