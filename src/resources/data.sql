80c1867461ce7d0849bc56b15521deac30cb6a82
Adriana Arteaga <arteaga.adriana@hotmail.com>
2019-10-01 22:15:28 +0200

    added recources

---
+++ src/resources/data.sql
@@ -0,0 +1,24 @@
+/*insert into tbl_permission(name)
+values('Admin'), ('User');
+
+insert into tbl_user(firstname, lastna me, email, pwd, permission_fk)
+values ('Adriana', 'Arteaga', 'arteaga.adriana@hotmail.com', 'asdf', 1),
+('Test', 'User', 'test@user.com', 'asdf', 2);
+
+insert into tbl_project(name, description, user_fk)
+values('IMPORTANT!!!', 'Some important stuff for this week', 1),
+('Buy', '', 1),
+('School', 'Homework', 2),
+('Work', '', 2);
+
+insert into tbl_list(name, description, project_fk)
+values('Household', '', 1),
+('English', '', 3),
+('Economy', '', 3),
+('Project', '', 4)
+
+insert into tbl_entry(name, description, list_fk)
+values('Page 73, Task 5', '', 2),
+('Management Summary', '', 3),
+('Logo', '', 3),
+('Contact PL', 'PL = Project Leader', 4)*/
